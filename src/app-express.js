7a6b8cd18f5335ba4e4926c9e2c990f98f2c8043
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-09-21 18:32:29 +0200

    Add node and express app

---
+++ src/app-express.js
@@ -0,0 +1,38 @@
+const express = module.require("express");
+const path = module.require("node:path");
+const fs = module.require("node:fs/promises");
+
+const app = express();
+const port = 8000;
+
+app.use(express.static("public"));
+
+app.use(async (req, res, next) => {
+  try {
+    let file = req.url === "/" ? "index" : req.url;
+    file = path.join(__dirname, "../public", `${file}.html`);
+    req.body = await fs.readFile(file, "utf8");
+    next();
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      const errorFile = path.join(__dirname, "../public", `404.html`);
+      req.body = await fs.readFile(errorFile, "utf8");
+      res.status(404);
+      next();
+    } else {
+      next(err);
+    }
+  }
+});
+
+app.get(["/", "/about", "contact-me"], (req, res) => {
+  res.end(req.body);
+});
+
+app.use((req, res) => {
+  res.end(req.body);
+});
+
+app.listen(port, () =>
+  console.log(`server listening on http://localhost:${port}`)
+);
---
+++ src/app-node.js
@@ -0,0 +1,27 @@
+const http = module.require("node:http");
+const path = module.require("node:path");
+const fs = module.require("node:fs");
+
+const port = 8000;
+
+const server = http.createServer(processRequest);
+server.listen(port, () =>
+  console.log(`server listening on http://localhost:${port}`)
+);
+
+function processRequest(req, res) {
+  let file = req.url === "/" ? "index" : req.url;
+  file = path.join(__dirname, "../public", `${file}.html`);
+
+  fs.readFile(file, (err, data) => {
+    if (err) showError(res);
+    else res.end(data);
+  });
+}
+
+function showError(res) {
+  const file = path.join(__dirname, "../public", `404.html`);
+  fs.readFile(file, (err, data) => {
+    res.end(data);
+  });
+}
