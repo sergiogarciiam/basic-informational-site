1281d8c6f5cedd9f1005fc34faecc3ee792226a9
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-09-22 19:10:14 +0200

    Update port

--- src/express-app.js
+++ src/express-app.js
@@ -3,7 +3,7 @@ const path = module.require("node:path");
 const fs = module.require("node:fs/promises");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT ?? 8080;
 
 app.use(express.static("public"));
 
